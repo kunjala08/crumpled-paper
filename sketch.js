bbd574546ed4bb7ffa103bc32017f0a2a341837b
kunjala08 <71957334+kunjala08@users.noreply.github.com>
2020-12-20 13:17:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ground;
+var ball
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+	ground=createSprite(400,640,800,20);
+    ball=new paper(50,50,25);
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ball.display();
+  drawSprites();
+ 
+}
+
+
+
